f85b8e2fa2f3119ec86fcf311705448dd06032bd
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-03 20:22:41 +0100

    Cleaned up and setup the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+import 'package:weatherpulse/screens/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
