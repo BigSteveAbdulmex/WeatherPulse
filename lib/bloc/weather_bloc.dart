7b63216a1d370849ad5610387d900de2aabae1d6
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-04 22:02:25 +0100

    Refactored the bloc

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
+import 'package:weatherpulse/data/app_data.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
@@ -12,17 +12,16 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
     on<FetchWeather>((event, emit) async {
       emit(WeatherBlocLoading());
       try {
-        await dotenv.load(fileName: ".env");
         WeatherFactory wf = WeatherFactory(
-          "${dotenv.env["APIKey"]}",
+          API_KEY,
           language: Language.ENGLISH,
         );
         Weather weather = await wf.currentWeatherByLocation(
           event.position.latitude,
           event.position.longitude,
         );
-        print(weather);
         emit(WeatherBlocSuccess(weather));
+        print(weather);
       } catch (e) {
         emit(WeatherBlocFailure());
       }
