5ac6745690cd4ef47f2f1981228572bcee2c0aff
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-04 20:26:25 +0100

    Implemented the weather bloc

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,13 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        Position position = await Geolocator.getCurrentPosition();
+        WeatherFactory wf = WeatherFactory(
+          '_apiKey',
+          language: Language.ENGLISH,
+        );
+        Weather weather = await wf.currentWeatherByLocation(
+          position.latitude,
+          position.longitude,
+        );
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
