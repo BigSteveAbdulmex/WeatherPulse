03658caf922374294019661bc509447b1a011d29
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-04 20:49:56 +0100

    Wrapped the main file with bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:weatherpulse/bloc/weather_bloc.dart';
 import 'package:weatherpulse/screens/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: BlocProvider<WeatherBloc>(
+        create: (context) => WeatherBloc()
+          ..add(
+            FetchWeather(),
+          ),
+        child: HomeScreen(),
+      ),
     ),
   );
 }
